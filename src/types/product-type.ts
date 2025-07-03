707deb7d5dae829f218d2fcb1d8d0f7128ec596f
leonardo-lim <leonardolim178@gmail.com>
2025-07-03 13:35:54 +0700

    feat: add product type

---
+++ src/types/product-type.ts
@@ -0,0 +1,15 @@
+interface ProductType {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: {
+        rate: number;
+        count: number;
+    };
+    quantity?: number;
+}
+
+export type { ProductType };
